86659299994845ea9d56f19bbe9f9c01b3a9814b
davidcechak <davidcechak@gmail.com>
2025-03-10 18:52:13 +0100

    train miRBind with original params: clean bash scripts

--- analysis/miRBind_CNN_retraining_orig_parameters/run_data_encoding.sh
+++ analysis/miRBind_CNN_retraining_orig_parameters/run_data_encoding.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
 
-# train_file = "manakov/AGO2_eCLIP_Manakov2022_train.tsv"
-# test_file_1 = "manakov/AGO2_eCLIP_Manakov2022_test.tsv"
-# test_file_2 = "manakov/AGO2_eCLIP_Manakov2022_leftout.tsv"
-DATASET="../../data/chimeric_datasets/Manakov2022_flat/AGO2_eCLIP_Manakov2022_test.tsv"
-# DATASET="../../data/chimeric_datasets/Manakov2022_flat/AGO2_eCLIP_Manakov2022_train.tsv"
-# DATASET="../../data/chimeric_datasets/Manakov2022/AGO2_eCLIP_Manakov2022_1_train_dataset.tsv"
-# DATASET="../../data/chimeric_datasets/Manakov2022/AGO2_eCLIP_Manakov2022_1_test_dataset.tsv"
+TEST_DATASET="../../data/chimeric_datasets/Manakov2022_flat/AGO2_eCLIP_Manakov2022_test.tsv"
+LEFTOUT_DATASET="../../data/chimeric_datasets/Manakov2022_flat/AGO2_eCLIP_Manakov2022_leftout.tsv"
+TRAIN_DATASET="../../data/chimeric_datasets/Manakov2022_flat/AGO2_eCLIP_Manakov2022_train.tsv"
 
 
-DATASET_OUT="encoded_dataset/Manakov2022_flat/AGO2_eCLIP_Manakov2022_1_test"
-# DATASET_OUT="encoded_dataset/Manakov2022_flat/AGO2_eCLIP_Manakov2022_1_train"
-# DATASET_OUT="encoded_dataset/AGO2_eCLIP_Manakov2022_1_test"
+TEST_DATASET_OUT="encoded_dataset/Manakov2022_flat/AGO2_eCLIP_Manakov2022_1_test"
+LEFTOUT_DATASET_OUT="encoded_dataset/Manakov2022_flat/AGO2_eCLIP_Manakov2022_1_leftout"
+TRAIN_DATASET_OUT="encoded_dataset/Manakov2022_flat/AGO2_eCLIP_Manakov2022_1_train"
 
 
 CODE="../../code/machine_learning"
 
 mkdir -p encoded_dataset/Manakov2022_flat
 
-# encode dataset
-python $CODE/encode/binding_2D_matrix_encoder.py --i_file $DATASET --o_prefix $DATASET_OUT
+# encode datasets
+python $CODE/encode/binding_2D_matrix_encoder.py --i_file $TEST_DATASET --o_prefix $TEST_DATASET_OUT
+python $CODE/encode/binding_2D_matrix_encoder.py --i_file $LEFTOUT_DATASET --o_prefix $LEFTOUT_DATASET_OUT
+python $CODE/encode/binding_2D_matrix_encoder.py --i_file $TRAIN_DATASET --o_prefix $TRAIN_DATASET_OUT
