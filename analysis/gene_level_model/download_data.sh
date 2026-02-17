9a3737a7723ea9b4340c0491db0d424bf3ba250e
davidcechak <davidcechak@gmail.com>
2026-02-17 20:06:48 +0000

    gene model: add data download

---
+++ analysis/gene_level_model/download_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python ../../code/shared/download_gdrive.py 1lbEzxHDs_LSqvdEizQII2lFVbKceH27S -o ../../data/Agarwal2015_train_data
+
+python ../../code/shared/download_gdrive.py 1Nlmkd7g7ZZg5GlbEuAAVTzced_ctGAaS -o ../../data/3utr_hg19 -n 3utr.sequences.refseq_id.mirna_fc.seed_cls.sequence.signal.conservation.seed_cls.ts_preds.full.pkl
