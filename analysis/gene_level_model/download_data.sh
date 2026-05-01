47b630519c02c6162373f3eaa2541d4d01643eab
davidcechak <davidcechak@gmail.com>
2026-05-01 16:56:53 +0200

    Add command to download pretrained gene model

--- analysis/gene_level_model/download_data.sh
+++ analysis/gene_level_model/download_data.sh
@@ -5,3 +5,5 @@ python ../../code/shared/download_gdrive.py 1tGmuhABqkpDCIyrgcVAPJs8123dyIleH -o
 python ../../code/shared/download_gdrive.py 1lbEzxHDs_LSqvdEizQII2lFVbKceH27S -o ../../data/Agarwal2015_train_data
 
 python ../../code/shared/download_gdrive.py 1Nlmkd7g7ZZg5GlbEuAAVTzced_ctGAaS -o ../../data/3utr_hg19 -n 3utr.sequences.refseq_id.mirna_fc.seed_cls.sequence.signal.conservation.seed_cls.ts_preds.full.pkl
+
+python ../../code/shared/download_gdrive.py 1x9aPAS8nF8PRqwIIlUiXbXdTeOuIyyyq -o gene_model_v4_pretrained_3conv
